b3e1afbcd4cfaae28ad38ce0adf1847177668acd
Gabrielle Bellini <gbellini90@gmail.com>
2018-09-25 20:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -141,9 +141,13 @@ function moveDodger(e) {
   document.addEventListener('keydown',function(e){
   if (e.which===LEFT_ARROW){
     moveDodgerLeft()
+    e.stopPropagation();
+		e.preventDefault();
   }
   if (e.which===RIGHT_ARROW){
     moveDodgerRight()
+    e.stopPropagation();
+		e.preventDefault();
   }
   })
 }
